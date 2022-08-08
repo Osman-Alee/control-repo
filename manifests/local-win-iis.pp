8b27aa516aecce441f187b2a6b8c71a061e408e2
Osman-Alee <merasohna000@gmail.com>
2022-08-08 12:44:32 +0500

    commit#2 changing the manifest file

--- manifests/local-win-iis.pp
+++ manifests/local-win-iis.pp
@@ -1,4 +1,4 @@
-node 'puppet-agent01'{
+node 'local-win-iis'{
 include chocolatey
 exec { 'save_dir':
         path => 'C:/Windows/System32',
@@ -9,86 +9,4 @@ exec { 'Install-Choco':
         unless => 'if (!(choco)){exit 1}',
         provider  => 'powershell',
   }
-################################################## New code added for IIS (22-6-2022) ########################################
-file { 'c:/Users/Administrator/interpayssl.pfx':
-     ensure => 'file',
-     source => 'puppet:///modules/sslcertificate/interpayssl.pfx',
-#     source_permissions => 'ignore',
-     owner  => 'administrator',
-     group  => ['administrators'],
-     mode   => '1777',
-}
-sslcertificate { 'Install-PFX-Certificate' :
-  name       => 'interpayssl.pfx',
-  password   => '12344321',
-  location   => 'c:\Users\Administrator',
-  thumbprint => '84AAD9F0AFDF02B4032D50F17D32043D9E30E3CC',
-  }
-# Create Directories
-
-file { 'c:\\inetpub\\complete':
-  ensure => 'directory'
-}
-
-file { 'c:\\inetpub\\complete_vdir':
-  ensure => 'directory'
-}
-
-# Set Permissions
-
-acl { 'c:\\inetpub\\complete':
-  permissions => [
-    {'identity' => 'System', 'rights' => ['read', 'execute']},
-  ],
-}
-
-acl { 'c:\\inetpub\\complete_vdir':
-  permissions => [
-    {'identity' => 'System', 'rights' => ['read', 'execute']},
-  ],
-}
-
-# Configure IIS
-
-iis_application_pool { 'complete_site_app_pool':
-  ensure                  => 'present',
-  state                   => 'started',
-  managed_pipeline_mode   => 'Integrated',
-  managed_runtime_version => 'v4.0',
-}
-
-# Application Pool No Managed Code .Net CLR Version set up
-iis_application_pool {'test_app_pool':
-    ensure                    => 'present',
-    enable32_bit_app_on_win64 => true,
-    managed_runtime_version   => '',
-    managed_pipeline_mode     => 'Classic',
-    start_mode                => 'AlwaysRunning'
-  }
-
-iis_site { 'mysite.interpayafrica.com':
-  ensure           => 'started',
-  physicalpath     => 'c:\\inetpub\\complete',
-  applicationpool  => 'complete_site_app_pool',
-  enabledprotocols => 'https',
-  bindings         => [
-    {
-      'bindinginformation'   => '*:443:',
-      'protocol'             => 'https',
-      'certificatehash'      => '84AAD9F0AFDF02B4032D50F17D32043D9E30E3CC',
-      'certificatestorename' => 'My',
-     'sslflags'             => 0,
-    },
-  ],
-  require => File['c:\\inetpub\\complete'],
-}
-
-iis_virtual_directory { 'vdir':
-  ensure       => 'present',
-  sitename     => 'mysite.interpayafrica.com',
-  physicalpath => 'c:\\inetpub\\complete_vdir',
-  require      => File['c:\\inetpub\\complete_vdir'],
-}
-
-################################################## IIS code ends here ########################################################
 }
